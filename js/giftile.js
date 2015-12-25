ef8cc83907186a6530793d3384c439f53a7e77d8
Benjamin Mintz <bmintz@users.noreply.github.com>
2015-12-25 15:23:04 -0600

    Completely removed jQuery!

--- js/giftile.js
+++ js/giftile.js
@@ -1,32 +1,56 @@
-$(document).ready(function () {
-
+document.addEventListener("DOMContentLoaded", function(event) {
     /* BEGIN NON-PARSE CODE */
 
     // Function that returns the URLs
     function getUrls() {
         URLs = {};
-        URLs.imgUrl = $("#imgUrlEl").val(),
-        URLs.audioUrl = $("#audioUrlEl").val();
+        URLs.imgUrl = document.getElementById("imgUrlEl").value;
+        URLs.audioUrl = document.getElementById("audioUrlEl").value;
         return URLs;
     }
-	// Don't refresh on form submit
-	$('#updateForm').submit(function () {
-		return false;
-	});
+    // Don't refresh on form submit
+    var formExists = false;
+    var form = document.getElementById('updateForm');
+    if (typeof(form) != 'undefined' && form !== null) {
+        form.onsubmit = function() {
+            return false;
+        };
+        formExists = true;
+    }
     //  Update the page with the given URLs
     function update(imgUrl, audioUrl) {
         // Use fixed CSS variable
         document.body.style.backgroundImage = 'url(' + imgUrl + ')';
-        var audioElement = $("#bgAudio");
-        audioElement.attr('src', audioUrl);
-    };
+        document.getElementById("bgAudio").setAttribute('src', audioUrl);
+    }
     //  Update the page with user-inputted URLs when #updateButton is clicked
-    $("#updateButton").click(function () {
-        // Get the URLs from the form
-        URLs = getUrls();
-        // Actually update the page
-        update(URLs.imgUrl, URLs.audioUrl);
-    });
+    button = document.getElementById("updateButton");
+    if (typeof(button) != "undefined" && button !== null) {
+        button.addEventListener("click", (function () {
+            // Get the URLs from the form
+            URLs = getUrls();
+            // Actually update the page
+            update(URLs.imgUrl, URLs.audioUrl);
+        }));
+    }
+    // http://stackoverflow.com/a/987376
+    function SelectText(element) {
+        var doc = document
+            , text = doc.getElementById(element)
+            , range, selection
+        ;    
+        if (doc.body.createTextRange) {
+            range = document.body.createTextRange();
+            range.moveToElementText(text);
+            range.select();
+        } else if (window.getSelection) {
+            selection = window.getSelection();        
+            range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
     /* END NON-PARSE CODE */
 
     /* BEGIN PARSE CODE */
@@ -36,47 +60,54 @@ $(document).ready(function () {
     var ShortURLs = Parse.Object.extend("ShortURLs");
     var shortUrls = new ShortURLs();
     var query = new Parse.Query(ShortURLs);
+
     // Create a permalink with the user-inputted URLs
     // Grab the HTTP GET query
-    var queryDict = {}
-    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+    // http://stackoverflow.com/a/21210643
+    var queryDict = {};
+    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
     // Detect permalinks!
-    if (typeof queryDict.id !== "undefined") {
+    if (typeof queryDict.id != "undefined" && queryDict.id !== null) {
         // Get the URLs from the requested ShortURLs id (without a trailing slash) if possible
         query.get(queryDict.id.replace(/\/+$/, ''), {
             success: function(object) {
-				// Make it clear to the user what image / audio is being used.
-				$("#imgUrlEl").val(object.get("imgUrl"));
-				$("#audioUrlEl").val(object.get("audioUrl"));
+                // Make it clear to the user what image / audio is being used.
+                if (formExists) {
+                    document.getElementById("imgUrlEl").setAttribute("value", object.get("imgUrl"));
+                    document.getElementById("audioUrlEl").setAttribute("value", object.get("audioUrl"));
+                }
                 update(object.get("imgUrl"), object.get("audioUrl"));
-                
             },
             error: function(object, error) {
                 // The object was not retrieved successfully.
                 // error is a Parse.Error with an error code and message.
+                alert("Failed to retrieve the permalink! Please check your typing and try again.");
             }
         });
-    };
+    }
     var hasPermalinkDisplayed = false;
-    $("#createPermalinkButton").click(function () {
-        URLs = getUrls();
-        if ((URLs.imgUrl !== "") || (URLs.audioUrl !== "")) {
-            shortUrls.save({
-                "imgUrl": URLs.imgUrl,
-                "audioUrl": URLs.audioUrl
-            }).then(function (object) {
-                if (hasPermalinkDisplayed == false) {
-                    newLink = window.location.origin + "/combo?id="+ shortUrls.id;
-                    $('#createPermalinkButton').after('<br />Your permalink is:<br /><input id="shortUrl" type="text" value="'+newLink+'" readonly>');
-                    $('#shortUrl').select();
-                    hasPermalinkDisplayed = true;
-                };
-            }, function (error) {
-                alert("Permalink save unsuccesful.\nPlease try again.");
-            });
-        } else {
-          alert("Permalink not saved. Please enter an image URL or an audio URL or both.");
-      };
-  });
+    if (formExists) {
+        document.getElementById("createPermalinkButton").addEventListener("click", (function () {
+            URLs = getUrls();
+            if ((URLs.imgUrl !== "") || (URLs.audioUrl !== "")) {
+                shortUrls.save({
+                    "imgUrl": URLs.imgUrl,
+                    "audioUrl": URLs.audioUrl
+                }).then(function (object) {
+                    if (hasPermalinkDisplayed === false) {
+                        newLink = window.location.origin + "/combo?id="+ shortUrls.id;
+                        document.getElementById('createPermalinkButton').insertAdjacentHTML('afterend',
+                            '<br />Your permalink is:<br /><a id="shortUrl" href="'+newLink+'">'+newLink+'</a>');
+                        SelectText('shortUrl');
+                        hasPermalinkDisplayed = true;
+                    }
+                }, function (error) {
+                    alert("Permalink save unsuccesful.\nPlease try again.");
+                });
+            } else {
+              alert("Permalink not saved. Please enter an image URL or an audio URL or both.");
+            }
+        }));
+    }
 });
 /* END PARSE CODE */
